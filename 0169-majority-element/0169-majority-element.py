8cf55d003be3977ae4c3c2c5f3ce30bb27357dcb
Abdulsamet KILIÇ <96330083+Abdulsametklc@users.noreply.github.com>
2025-08-23 15:48:01 +0300

    Time: 14 ms (32.46%), Space: 14.6 MB (10.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums_u= list(dict.fromkeys(nums))
+        if len(nums) == 1 or len(set(nums))==1:
+            return nums[0]
+        s_nums = list(set(nums))
         uzunluk = len(nums)
-        uzunluk_u = len(nums_u)
-        max_sayac = 0
-        a=None
-
-        for i in range(uzunluk_u):
+        uzunluk_s = len(s_nums)
+        sayac_list = []
+        for i in range(uzunluk_s):
             sayac = 0
             for j in range(uzunluk):
-                if nums_u[i] == nums[j]:
+                if s_nums[i] == nums[j]:
                     sayac +=1
-
-            if sayac > max_sayac:
-                max_sayac = sayac
-                a = nums_u[i]
-
-        return a                        
+            sayac_list.append(sayac)
+        en_buyuk = sayac_list[0]
+        indeks = 0
+        for k in range(1,uzunluk_s):
+            if en_buyuk < sayac_list [k]:
+                en_buyuk = sayac_list[k]
+                indeks = k
+        return s_nums[indeks]
