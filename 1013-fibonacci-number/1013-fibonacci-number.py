590a6db10981c43dd7abd7e51060659758b291b3
Abdulsamet KILIÇ <96330083+Abdulsametklc@users.noreply.github.com>
2025-09-05 11:24:38 +0300

    Time: 11 ms (91.4%), Space: 12.5 MB (51.2%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,13 +2,15 @@ class Solution(object):
     def fib(self, n):
         if n==0 or n==1:
             return n
-        toplam1=0
-        toplam2=1
-        toplam3=0
-        for i in range(n-1):
-            toplam3 = toplam2 + toplam1
-            toplam1 = toplam2
-            toplam2 = toplam3
-        return toplam3
+        x,y = 0,1
+        for i in range(n):
+            x, y = y, x+y 
+        return x
 
-        
+'''Farklı öğrendiğim yöntemle
+class Solution(object):
+    def fib(self,n):
+        if n==0 or n==1:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)'''
