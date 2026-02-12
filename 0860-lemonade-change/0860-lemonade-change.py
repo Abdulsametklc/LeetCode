caff73c3c3b2f769656cc9b7594dd06197b1e65c
Abdulsamet KILIÇ <96330083+Abdulsametklc@users.noreply.github.com>
2026-02-12 12:36:57 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        para = {}
+        para[5], para[10] = 0, 0
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                para[5] += 1
+            elif bills[i] == 10:
+                para[10] += 1
+                if para[5] == 0:
+                    return False
+                else:
+                    para[5] -= 1
+            else:
+                if para[5] == 0 or para[10] == 0:
+                    return False
+                else:
+                    para[5] -= 1 
+                    para[10] -= 1
+        return True
